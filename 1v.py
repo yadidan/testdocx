7e548dad4e9015c2f5eaf21613c34434b7764952
Yaroslav <58103185+yadidan@users.noreply.github.com>
2019-12-22 21:22:25 +0600

    Add files via upload

---
+++ 1v.py
@@ -0,0 +1,38 @@
+from docx import Document
+from random import randint
+k=set()
+wordDoc = Document('us.docx')
+global mas
+mas=None
+global h
+h=0
+d=[randint(1,99) for x in range(20)]
+tables_size =wordDoc.tables
+print(wordDoc.tables[149].rows[0].cells[0].text)
+for i in d:
+     mas=[]
+     for j in range(1,6):
+         if wordDoc.tables[i].rows[j].cells[0].text == '1':
+                  k.add('f'+wordDoc.tables[i].rows[j].cells[-1].text)
+
+             
+         else:
+             k.add(wordDoc.tables[i].rows[j].cells[1].text)
+     print(wordDoc.tables[i].rows[0].cells[-1].text)
+     for z in k:
+         h+=1
+         if z[0]=='f':
+             print(f'ответ {h} : {z[1:len(z)]}')
+             mas.append(h)     
+         else:
+                print(f'ответ {h} : {z}')
+     n=int(input('Введите ответ : '))
+     if n in mas   :
+         print('Все верно ')
+     else:
+        print('правильно так : ')
+        print(mas)
+        
+     h=0
+     k=set()
+     mas=None
---
+++ 2v.py
@@ -0,0 +1,39 @@
+from docx import Document
+from random import randint
+k=set()
+wordDoc = Document('us.docx')
+global mas
+mas=None
+global h
+h=0
+d=[randint(100,150) for x in range(20)]
+tables_size =wordDoc.tables
+print(wordDoc.tables[149].rows[0].cells[0].text)
+for i in d:
+     mas=[]
+     for j in range(1,7):
+         if wordDoc.tables[i].rows[j].cells[0].text == '1':
+                  k.add('f'+wordDoc.tables[i].rows[j].cells[-1].text)
+
+             
+         else:
+             k.add(wordDoc.tables[i].rows[j].cells[1].text)
+     print(wordDoc.tables[i].rows[0].cells[-1].text)
+     for z in k:
+         h+=1
+         if z[0]=='f':
+             print(f'ответ {h} : {z[1:len(z)]}')
+             mas.append(h)     
+         else:
+                print(f'ответ {h} : {z}')
+     n=[int(x) for x in input('Введите ответ : ').split()]
+     if n[0] in mas  and n[1] in mas  :
+         print('Все верно ')
+     else:
+        print('правильно так : ')
+        print(mas)
+        
+     h=0
+     k=set()
+     mas=None
+
